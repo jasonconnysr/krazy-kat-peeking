bb986207a144c6697d6de14ad39b88cc95b35570
Jason Conny <jconny@shoprunner.com>
2019-11-27 08:14:29 -0600

    wire up mock express server

---
+++ api/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+import cors from 'cors';
+
+const port = 8080;
+const mockAPI: express.Application = express();
+const corsOptions = {
+    origin: true,
+    exposedHeaders: ['Access-Control-Allow-Origin'],
+    methods: ['get', 'post',],
+    optionsSuccess: 200
+};
+
+mockAPI.use(bodyParser.json());
+
+mockAPI.options('*', cors());
+
+mockAPI.get('/api/member/:memberId', cors(corsOptions), (req, res) => {
+    const responsePath = `${__dirname}/json/mockMember.json`;
+    const response = fs.createReadStream(responsePath);
+    response.pipe(res);
+});
+
+mockAPI.get('/api/member/:memberId/membership', cors(corsOptions), (req, res) => {
+    const responsePath = `${__dirname}/json/mockMembership.json`;
+    const response = fs.createReadStream(responsePath);
+    response.pipe(res);
+});
+
+mockAPI.get('/api/member/:memberId/orders', cors(corsOptions), (req, res) => {
+    const responsePath = `${__dirname}/json/mockOrders.json`;
+    const response = fs.createReadStream(responsePath);
+    response.pipe(res);
+});
+
+mockAPI.listen(port);
